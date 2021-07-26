d7d7d1f4e340d023c7434bf888605c2419676968
Brian Clark <brian.w.clark176@gmail.com>
2021-07-26 15:59:02 -0600

    Axios library API request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -7,17 +8,19 @@ function App() {
   const [people, setPeople] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-      .then(response => response.json())
-      .then(response => setPeople(response.results))
+    // fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=200')
+    //   .then(response => response.json())
+    //   .then(response => setPeople(response.results))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=200')
+      .then(response => { setPeople(response.data.results) })
   }, []);
-
 
   return (
     <div className="App">
       {people.length > 0 && people.map((person, index) => {
+        console.log(index)
         return (<div key={index}>
-          <ul style={{ marginLeft: 70 }}><li>{person.name}</li></ul></div>)
+          <ul style={{ marginLeft: 110 }}><li>{person.name}</li></ul></div>)
       })}
     </div>
   );
