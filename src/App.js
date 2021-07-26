8c5518a5445e9b63d60dd9b5d8c7229aa3b44d0c
Brian Clark <brian.w.clark176@gmail.com>
2021-07-26 14:12:18 -0600

    RESTful API call for a list of 807 Pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+      .then(response => response.json())
+      .then(response => setPeople(response.results))
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {people.length > 0 && people.map((person, index) => {
+        return (<div key={index}>
+          <ul style={{ marginLeft: 70 }}><li>{person.name}</li></ul></div>)
+      })}
     </div>
   );
 }
